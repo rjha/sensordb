693350c56030dba49c76f9fb27111fd1e1d7c866
Rajeev Jha <jha.rajeev@gmail.com>
2013-07-28 18:59:45 +0530

    + Added java.util.logging properties file for logger  same logger is used by glassfish hence we do not want to use  slf4j or log4j etc.

--- java/src/com/yuktix/rest/exception/ThrowableMapper.java
+++ java/src/com/yuktix/rest/exception/ThrowableMapper.java
@@ -1,5 +1,8 @@
 package com.yuktix.rest.exception;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
@@ -14,11 +17,12 @@ import com.yuktix.dto.ErrorBean;
 	 */
 	@Provider
 	public class ThrowableMapper implements ExceptionMapper<Throwable> {
-		
+		private static final Logger logger= Logger.getLogger(com.yuktix.rest.exception.ThrowableMapper.class.getName());
+
 	    @Override
 	    public Response toResponse(Throwable ex) {
-	    	// @todo log stack trace
-	    	ex.printStackTrace();
+	    	
+	    	logger.log(Level.SEVERE, ex.getMessage(),ex);
 	    	String message = "Internal service error" ;
 	    	
 	    	// jackson json parsing exception
