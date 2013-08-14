693c40e65afb096b607286934e543ec0124c79b3
rjha <jha.rajeev@gmail.com>
2013-08-14 22:16:02 +0530

    fixed names of test scripts

--- test/tsdb-latest.php
+++ test/tsdb-latest.php
@@ -1,9 +1,9 @@
 <?php
 	
     require("common.inc");
-	$post_url = "http://localhost:9090/sensordb/v1/query/sensor/latest" ;
+	$post_url = "http://localhost:9090/sensordb/v1/sensor/latest" ;
     $unix_ts = time();
-	$data = array("projectId" => "p001" , "serialNumber" => "sensor001", "size" => 10);
+	$data = array("projectId" => "p001" , "serialNumber" => "s001", "size" => 10);
 
 	$post_data = json_encode($data);
     $cookies = array("COOKIE1" => "ABCD1234" , "COOKIE2" => "magic1234");
@@ -11,7 +11,7 @@
     $http_code = $response["code"];
     $result = $response["data"];
 
-	if($http_code == 200 ) {
+	if($http_code == 200) {
 		printf("success \n \t response => \n" );
 		print_r($result);
 		printf(" \n \n ");
