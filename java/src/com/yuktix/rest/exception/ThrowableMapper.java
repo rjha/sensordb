fb262c59800eac4cfdc8f8f413f09d10ea4552ad
rjha <jha.rajeev@gmail.com>
2013-08-19 13:13:11 +0530

    Do not do logging for client errors

--- java/src/com/yuktix/rest/exception/ThrowableMapper.java
+++ java/src/com/yuktix/rest/exception/ThrowableMapper.java
@@ -23,25 +23,31 @@ import com.yuktix.util.Log;
 	    @Override
 	    public Response toResponse(Throwable ex) {
 	    	
-	    	Log.error(ex.getMessage(), ex);
 	    	String message = "Internal service error" ;
 	    	int code = Status.INTERNAL_SERVER_ERROR.getStatusCode() ;
+	    	boolean logIt = true ;
 	    	
 	    	// jackson json parsing exception
 	    	// @todo - jersey exception contains stack trace
 	    	
 	    	if(ex instanceof org.codehaus.jackson.JsonProcessingException) {
 	    		message = String.format("Bad json; json processing error");
+	    		logIt = false ;
 	    	}
 	    	
 	    	if(ex instanceof org.glassfish.jersey.server.ParamException) {
 	    		message = String.format("Bad request parameter : %s ", ((org.glassfish.jersey.server.ParamException) ex).getParameterName());
+	    		logIt = false ;
 	    	}
 	    	
 	    	if(ex instanceof javax.ws.rs.ClientErrorException) {
 	    		message = ex.getMessage();
 	    		code  = ((javax.ws.rs.ClientErrorException) ex).getResponse().getStatus();
-	    		
+	    		logIt = false ;
+	    	}
+	    	
+	    	if(logIt) {
+	    		Log.error(ex.getMessage(), ex);
 	    	}
 	    	
 	        return Response.status(code)
