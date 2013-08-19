62aa38993e80c51b31be003abfb4c40dda2709d2
rjha <jha.rajeev@gmail.com>
2013-08-19 13:14:37 +0530

    Added debug log flag to logger

--- java/src/com/yuktix/util/Log.java
+++ java/src/com/yuktix/util/Log.java
@@ -1,10 +1,30 @@
 package com.yuktix.util;
 
+import java.util.Locale;
+import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Log {
 	private static final Logger logger= Logger.getLogger(com.yuktix.util.Log.class.getName());
+	public static boolean isDebug ;
+	
+	static {
+		
+		try {
+			
+			ResourceBundle bundle = ResourceBundle.getBundle("sensordb",Locale.US);
+			String debug = bundle.getString("sensordb.debug.log");
+			Log.isDebug = Boolean.parseBoolean(debug);
+			 
+			
+		} catch(Exception ex) {
+			String message = "error reading sensordb debug log" ;
+			Log.error(message, ex);
+			Log.isDebug = false ;
+			
+		}
+	}
 	
 	public static void error(String message) {
 		logger.log(Level.SEVERE, message);
